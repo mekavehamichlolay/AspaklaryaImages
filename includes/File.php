962b55900ba888d92a4ba219ed7d37d4edbe6ff8
mekavehamichlol <mekavehamichlol@gmail.com>
2025-11-17 13:39:25 +0200

    Add status bit management methods to File class

--- includes/File.php
+++ includes/File.php
@@ -16,10 +16,33 @@ class File {
 
     private Title $title;
 
+    private int $statusBits = 0;
+
     public function __construct( string $title ) {
         $this->title = FileRepoFile::normalizeTitle( $title );
         if ( !$this->title || !$this->title->canExist()) {
             throw new RuntimeException( "`$title` is not a valid file title." );
         }
+    }
+
+    public function setStatusBits( int $bits ): void {
+        $this->statusBits = $bits;
+    }
+
+    public function getStatusBits(): int {
+        return $this->statusBits;
+    }
+
+    public function isNetfreeKnown(): bool {
+        return ( $this->statusBits & self::NETFREE_KNOWN_BIT ) !== 0;
+    }
+    public function isNetfreeOpen(): bool {
+        return ( $this->statusBits & self::NETFREE_OPEN_BIT ) !== 0;
+    }
+    public function isAuthorizedKnown(): bool {
+        return ( $this->statusBits & self::AUTHORIZED_KNOWN_BIT ) !== 0;
+    }
+    public function isAuthorizedOpen(): bool {
+        return ( $this->statusBits & self::AUTHORIZED_OPEN_BIT ) !== 0;
     }
 }
