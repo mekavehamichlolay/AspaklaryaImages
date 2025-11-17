b4623dfb9636ade54a22b2dffeec4df1c8438734
mekavehamichlol <mekavehamichlol@gmail.com>
2025-11-17 10:17:09 +0200

    Add SchemaUpdater class for handling database schema updates

---
+++ includes/Hooks/SchemaUpdater.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MediaWiki\Extension\AspaklaryaImages\Hooks;
+
+use MediaWiki\Installer\DatabaseUpdater;
+use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
+
+class SchemaUpdater implements LoadExtensionSchemaUpdatesHook {
+    /**
+     * @param DatabaseUpdater $updater
+     */
+    public function onLoadExtensionSchemaUpdates( $updater ) {
+        $type = $updater->getDB()->getType();
+        $updater->addExtensionTable(
+            'aspaklarya_images',
+            __DIR__ . '/../../db/' . $type . '/tables-generated.sql'
+        );
+    }
+}
