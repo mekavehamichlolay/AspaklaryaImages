5e2285da8da7b0ab7089a7b843e70c92997ae68f
mekavehamichlol <mekavehamichlol@gmail.com>
2026-01-11 23:08:46 +0200

    Implement ImageBeforeProduceHTML hook for image status management

---
+++ includes/Hooks/Main.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace MediaWiki\Extension\AspaklaryaImages\Hooks;
+
+use MediaWiki\FileRepo\File\File;
+use MediaWiki\Hook\ImageBeforeProduceHTMLHook;
+use MediaWiki\Parser\Parser;
+use MediaWiki\Title\Title;
+use MediaWiki\Extension\AspaklaryaImages\File as AIFile;
+use Wikimedia\ObjectCache\WANObjectCache;
+use Wikimedia\Rdbms\ILoadBalancer;
+
+class Main implements ImageBeforeProduceHTMLHook {
+
+
+    public function __construct( private ILoadBalancer $loadBalancer, private WANObjectCache $cache ) {
+        
+    }
+    /**
+	 * This hook is called before producing the HTML created by a wiki image insertion.
+	 * You can skip the default logic entirely by returning false, or just modify a few
+	 * things using call-by-reference.
+	 *
+	 * @since 1.35
+	 *
+	 * @param null $unused Will always be null
+	 * @param Title &$title Title object of the image
+	 * @param File|false &$file File object, or false if it doesn't exist
+	 * @param array &$frameParams Various parameters with special meanings; see documentation in
+	 *   includes/Linker.php for Linker::makeImageLink
+	 * @param array &$handlerParams Various parameters with special meanings; see documentation in
+	 *   includes/Linker.php for Linker::makeImageLink
+	 * @param string|bool &$time Timestamp of file in 'YYYYMMDDHHIISS' string
+	 *   form, or false for current
+	 * @param string &$res Final HTML output, used if you return false
+	 * @param Parser $parser
+	 * @param string &$query Query params for desc URL
+	 * @param string &$widthOption Used by the parser to remember the user preference thumbnailsize
+	 * @return bool|void True or no return value to continue or false to skip the default logic
+	 */
+	public function onImageBeforeProduceHTML( $unused, &$title, &$file,
+		&$frameParams, &$handlerParams, &$time, &$res, $parser, &$query, &$widthOption
+	) {
+        $fileClass = new AIFile( $this->loadBalancer, $this->cache, $title->getPrefixedDBKey() );
+        $netfreeStatus = $fileClass->getNetfreeStatus();
+        $authorizedStatus = $fileClass->getAuthorizedStatus();
+        if ( $authorizedStatus === false ) {
+            $res = '';
+            return false;
+        }
+        if ( !isset( $frameParams['class'] ) ) {
+            $frameParams['class'] = '';
+        }
+        if ( $netfreeStatus === null ) {
+            $frameParams[ 'class' ] .= ' aspaklarya-images-netfree-unknown ';
+        } elseif ( !$netfreeStatus ) {
+            $frameParams[ 'class' ] .= ' aspaklarya-images-netfree-blocked ';
+        } 
+        return true;
+    }
+}
