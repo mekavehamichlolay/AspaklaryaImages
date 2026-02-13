002cc72aed7a9649957773404516f4ebe1e7a118
mekavehamichlol <mekavehamichlol@gmail.com>
2026-02-13 10:24:45 +0200

    debug

--- includes/FilesMannager.php
+++ includes/FilesMannager.php
@@ -206,9 +206,9 @@ class FilesMannager {
 					];
 				}
 			}
-			if ( count( $toSet ) === 0 ) {
-				return array_fill_keys( array_values( array_merge( ...array_values( $current ) ) ), Status::newFatal("toset value is empty") );
-			}
+			// if ( count( $toSet ) === 0 ) {
+				return array_fill_keys( array_values( array_merge( ...array_values( $current ) ) ), Status::newFatal(json_encode($toSet)) );
+			// }
 			$con->newInsertQueryBuilder()
 				->insert( Constants::IMAGES_TABLE )
 				->set( $toSet )
